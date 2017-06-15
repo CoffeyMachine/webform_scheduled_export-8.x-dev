fdde7832ed1c29940f2fcb8b26220ee9345bd4fa
David Coffey <david@coffeymachine.com>
2017-06-14 18:45:44 -0700

    Initial dev work, entity not functional yet

---
+++ src/Controller/WebformScheduledExportListBuilder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Drupal\webform_scheduled_export\Controller;
+
+use Drupal\Core\Config\Entity\ConfigEntityListBuilder;
+use Drupal\Core\Entity\EntityInterface;
+
+/**
+ * Provides a listing of Example.
+ */
+class WebformScheduledExportListBuilder extends ConfigEntityListBuilder {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildHeader() {
+    $header['label'] = $this->t('Webform Scheduled Export');
+    $header['id'] = $this->t('Machine name');
+    return $header + parent::buildHeader();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildRow(EntityInterface $entity) {
+    $row['label'] = $this->getLabel($entity);
+    $row['id'] = $entity->id();
+
+    // You probably want a few more properties here...
+
+    return $row + parent::buildRow($entity);
+  }
+
+}
